bd32933ed7c73d5af37bacb14f69de017d9c87f2
Kiran Rao <6110221+kranrao@users.noreply.github.com>
2024-08-20 10:59:19 -0700

    Add solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    // Primitive solution
+    // let string = "";
 
+    // for (let i = (words.length - 1); i >= 0; i--) {
+    //     string += words[i];
+    // }
+    
+    // return string;
+
+    // Abstracted solution
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
