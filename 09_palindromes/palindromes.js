66d369b4a6bb012d8d37db94deb07801da094e2e
Kiran Rao <6110221+kranrao@users.noreply.github.com>
2024-08-29 14:27:07 -0700

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz123456789";
 
+    // Remove case and non-alphanumeric characters from str
+    cleanStr = str
+                .toLowerCase()
+                .split("")
+                .filter(char => alphanumerical.includes(char))
+                .join("");
+    
+    // Create reversed copy of cleanStr
+    reversedCleanStr = cleanStr
+                        .split("")
+                        .reverse()
+                        .join("");
+                        
+    return cleanStr === reversedCleanStr;
 };
 
 // Do not edit below this line
