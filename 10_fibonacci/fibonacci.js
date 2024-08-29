8aab6d8498135285319589a040e1ee0c183e3698
Kiran Rao <6110221+kranrao@users.noreply.github.com>
2024-08-29 14:46:11 -0700

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+/*
+fib(1) = 1
+fib(2) = 1
+fib(3) = 2
+fib(4) = 3
+fib(5) = 5
+fib(6) = 8
+*/
 
+
+const fibonacci = function(num) {
+    if (+num === 0) return 0;
+    if (+num < 0) return "OOPS";
+    
+    // Base case: if num is 1 or 2, return 1
+    if (+num === 1 || +num === 2) return 1;
+
+    // Recursive case: if num greater than 2, return the sum of the preceding 2 fibs
+    return fibonacci(+num - 1) + fibonacci(+num - 2);
 };
 
 // Do not edit below this line
